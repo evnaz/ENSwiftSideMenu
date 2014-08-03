bc965a2f223a936ab7c2ec6a9d9ad2adb38c3630
Evgeny <nes@freshfactory.ru>
2014-08-03 14:50:39 +0600

    Initial Commit

---
+++ SwiftSideMenu/MenuTableViewController.swift
@@ -0,0 +1,53 @@
+//
+//  MenuController.swift
+//  SwiftSideMenu
+//
+//  Created by Evgeny on 23.07.14.
+//  Copyright (c) 2014 Evgeny Nazarov. All rights reserved.
+//
+
+import UIKit
+
+protocol MenuTableViewControllerDelegate {
+    func menuControllerDidSelectRow(indexPath:NSIndexPath)
+}
+
+class MenuTableViewController: UITableViewController {
+
+    var delegate : MenuTableViewControllerDelegate?
+    var tableData : Array<String> = []
+    
+    override func numberOfSectionsInTableView(tableView: UITableView!) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    override func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        
+        var cell = tableView.dequeueReusableCellWithIdentifier("CELL") as? UITableViewCell
+        
+        if !cell {
+            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "CELL")
+            cell!.backgroundColor = UIColor.clearColor()
+            cell!.textLabel.textColor = UIColor.darkGrayColor()
+            let selectedBackgroundView = UIView(frame: CGRectMake(0, 0, cell!.frame.size.width, cell!.frame.size.height))
+            selectedBackgroundView.backgroundColor = UIColor.grayColor().colorWithAlphaComponent(0.2)
+            cell!.selectedBackgroundView = selectedBackgroundView
+        }
+        
+        cell!.textLabel.text = tableData[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView!, heightForRowAtIndexPath indexPath: NSIndexPath!) -> CGFloat {
+        return 50.0
+    }
+    
+    override func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        delegate?.menuControllerDidSelectRow(indexPath)
+    }
+}
--- SwiftSideMenu/ViewController.swift
+++ SwiftSideMenu/ViewController.swift
@@ -8,18 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-                            
+class ViewController: UIViewController, SideMenuDelegate {
+    
+    var sideMenu : SideMenu?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        sideMenu = SideMenu(sourceView: self.view, menuData: ["UIDynamics", "UIGestures", "UIBlurEffect"])
+        sideMenu!.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func sideMenuDidSelectItemAtIndex(index: Int) {
+        sideMenu?.toggleMenu()
+    }
 
+    @IBAction func toggleSideMenu(sender: AnyObject) {
+        sideMenu?.toggleMenu()
+    }
+    
 
 }
 
