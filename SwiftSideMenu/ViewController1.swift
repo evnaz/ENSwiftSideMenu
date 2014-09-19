849f3e05ee0f39b7e06bdbeb13b4bf8ffd9c99bc
kNeerajPro <kn.neeraj.89@gmail.com>
2014-09-19 18:11:14 +0530

    xcode 6.0.1 update.Removing memory cycles.Data encapsulation using Swift Access Control modifiers.Adding center container view controller to set multiple view controllers for menu.Added 4 sample view controller for examples

---
+++ SwiftSideMenu/ViewController1.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwiftSideMenu
+//
+//  Created by Evgeny on 03.08.14.
+//  Copyright (c) 2014 Evgeny Nazarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController1: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    deinit {
+        NSLog("ViewController1 deinits")
+    }
+}
+
---
+++ SwiftSideMenu/ViewController4.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController4.swift
+//  SwiftSideMenu
+//
+//  Created by Neeraj Kumar on 19/09/14.
+//  Copyright (c) 2014 Evgeny Nazarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController4: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    deinit {
+        NSLog("ViewController4 deinits")
+    }
+
+}
