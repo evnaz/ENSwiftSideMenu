bad76926d73d1c186309258f72497f94a6a417ae
Ricardo Herrera <ricardo.herrera@santexgroup.com>
2015-11-03 15:45:29 -0500

    added dynamic framework

---
+++ Library/ENSwiftSideMenu/ENSwiftSideMenu/ENSwiftSideMenu.h
@@ -0,0 +1,18 @@
+//
+//  ENSwiftSideMenu.h
+//  ENSwiftSideMenu
+//
+//  Created by Ricardo Herrera on 11/3/15.
+//  Copyright © 2015 Ricardo Herrera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ENSwiftSideMenu.
+FOUNDATION_EXPORT double ENSwiftSideMenuVersionNumber;
+
+//! Project version string for ENSwiftSideMenu.
+FOUNDATION_EXPORT const unsigned char ENSwiftSideMenuVersionString[];
+
+
+
